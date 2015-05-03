b13243c14677693e167e84f72aa183696fbfee26
DennetM <DennetM.alt@gmail.com>
2015-05-03 11:05:44 +0200

    NOTHING IS RAINBOWS.
    Deleted everything because it made no sense. My understanding of
    performing a 2D (F)FT was flawed. Gotta rewrite everything from ground
    up to work correctly.
    
    Sorry!

--- Zadanie1/imageReadFunctions/FourierTransformation.java
+++ Zadanie1/imageReadFunctions/FourierTransformation.java
@@ -50,6 +50,7 @@ public class FourierTransformation extends ImageReading {
 	public FourierTransformation(String x){
 		super(x); //We-do what we did in the original.
 		
+		/*
 		//Initialize the table values.
 		int[][] tempR = new int[this.img.getWidth()][this.img.getHeight()];
 		int[][] tempG = new int[this.img.getWidth()][this.img.getHeight()];
@@ -79,6 +80,8 @@ public class FourierTransformation extends ImageReading {
 			this.b[i] = blist.get(i);
 		}
 		//At this point we have our RGB tables filled by rows. We can now get the data going.
+		 
+		 */
 	}
 	
 
@@ -89,6 +92,7 @@ public class FourierTransformation extends ImageReading {
 	//pad() - checks the length of R, G and B tables and compares them to the nearest power of 2.
 	// This is a requirement for FFT to work. If we don't fit, we jump higher and pad the tables with 0s.
 	private void pad(){
+		/*
 		//This is the power index (aka the power value) we're looking for.
 		int powerIndex = 1;
 		//While the index is lesser than the number we're dealing with AND lower than 2^23 (because come on I make pics that big sometimes).
@@ -103,37 +107,41 @@ public class FourierTransformation extends ImageReading {
 		this.r = Arrays.copyOf(r, powerIndex);
 		this.g = Arrays.copyOf(g, powerIndex);
 		this.b = Arrays.copyOf(b, powerIndex);
+		*/
 	}
 	
 	//Updates the separate tables (the ones that split Real and Acid Trips for purposes of display and clarity).
 	//This is invoked a lot. A LOT.
 	private void updateSeparates(){
-	//(Fun fact - all lengths are the same, I just put them differently for flavour purposes.)
-	rRe = new double[rCom.length];
-	rIm = new double[rCom.length];
-	gRe = new double[gCom.length];
-	gIm = new double[gCom.length];
-	bRe = new double[bCom.length];
-	bIm = new double[bCom.length];
-	//And fill them.
-	for(int i=0; i<rCom.length; i++){
-		rRe[i] = rCom[i].getReal();
-		rIm[i] = rCom[i].getImaginary();
-		//Debug for red:
-		//System.out.println("Real Red["+i+"]: "+rRe[i]);
-		//System.out.println("Imaginary Red["+i+"]: "+rIm[i]);
-		
-		gRe[i] = gCom[i].getReal();
-		gIm[i] = gCom[i].getImaginary();
-		
-		bRe[i] = bCom[i].getReal();
-		bIm[i] = bCom[i].getImaginary();
-	}
+	/*
+		//(Fun fact - all lengths are the same, I just put them differently for flavour purposes.)
+		rRe = new double[rCom.length];
+		rIm = new double[rCom.length];
+		gRe = new double[gCom.length];
+		gIm = new double[gCom.length];
+		bRe = new double[bCom.length];
+		bIm = new double[bCom.length];
+		//And fill them.
+		for(int i=0; i<rCom.length; i++){
+			rRe[i] = rCom[i].getReal();
+			rIm[i] = rCom[i].getImaginary();
+			//Debug for red:
+			//System.out.println("Real Red["+i+"]: "+rRe[i]);
+			//System.out.println("Imaginary Red["+i+"]: "+rIm[i]);
+			
+			gRe[i] = gCom[i].getReal();
+			gIm[i] = gCom[i].getImaginary();
+			
+			bRe[i] = bCom[i].getReal();
+			bIm[i] = bCom[i].getImaginary();
+		}
+	*/
 	}
 	
 	//The main standard FFT function.
 	//Type denotes the necessary type - either FORWARD (simple) or INVERSE.
 	public void standardFFT(TransformType type){
+		/*
 		//First thing's first - we need to pad stuff out.
 		pad();
 		//Now, we transform using the fft and its type. We get a single array of complex numbers aka EVIL.
@@ -145,6 +153,7 @@ public class FourierTransformation extends ImageReading {
 		//Initialize them.
 		updateSeparates();
 		//TO-DO Paula: Wyœwietlane widma mocy (rgbRe) i czêstotliwoœci (rgbIm) dla obrazu.
+		 */
 		
 	}
 	
@@ -154,6 +163,7 @@ public class FourierTransformation extends ImageReading {
 	
 	//The ur-highpass. Everything below the limit is PURGED.
 	public void filterHighpass(double limit){
+		/*
 		//Travel through the entire complex array.
 				for (int i=0; i<rCom.length; i++){
 					//Check if the REAL value is higher than borderofLife, and lower than borderofDeath (haha Perfect Cherry Blossom reference)
@@ -168,10 +178,12 @@ public class FourierTransformation extends ImageReading {
 					}
 				}
 		updateSeparates();
+		*/
 	}
 	
 	//The ur-lowpass. Ditto, just for above.
 	public void filterLowpass(double limit){
+		/*
 		//Travel through the entire complex array.
 		for (int i=0; i<rCom.length; i++){
 			//Check if the REAL value is higher than borderofLife, and lower than borderofDeath (haha Perfect Cherry Blossom reference)
@@ -186,10 +198,12 @@ public class FourierTransformation extends ImageReading {
 			}
 		}
 		updateSeparates();
+		*/
 	}	
 	
 	//The ur-bandpass filter. The user selects the lower border and the upper border and we purge (zero-ify) everything not between them.
 	public void filterBandpass(double borderofLife, double borderofDeath){
+		/*
 		//Travel through the entire complex array.
 		for (int i=0; i<rCom.length; i++){
 			//Check if the REAL value is higher than borderofLife, and lower than borderofDeath (haha Perfect Cherry Blossom reference)
@@ -204,5 +218,6 @@ public class FourierTransformation extends ImageReading {
 			}
 		}
 		updateSeparates();
+		*/
 	}
 }
